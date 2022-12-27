0a82e6fd1818835ac89136bcdea73d50a7b403eb
AKS <abhi144k@gmail.com>
2022-12-28 00:36:22 +0530

    28 commit

---
+++ app/Http/Controllers/API/CategoryController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $category = Category::all();
+        return response()->json([
+            'status' => 200,
+            'category' => $category,
+        ]);
+    }
+
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'meta_title' => 'required|max:191',
+            'slug' =>'required|max:191',
+            'name' => 'required|max:191',
+        ]);
+        if($validator->fails())
+        {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->messages(),
+            ]);
+        }
+        else
+        {
+            $category                 =   new Category();
+            $category->meta_title     =   $request->meta_title;
+            $category->meta_keyword   =   $request->meta_keyword;
+            $category->meta_desc      =   $request->meta_desc;
+            $category->slug           =   $request->slug;
+            $category->name           =   $request->name;
+            $category->description    =   $request->description;
+            $category->status         =   $request->input('status') == true ? '1':'0';
+            $category->save();
+
+            return response()->json([
+                'status' => 200,
+                'message' => 'Category added Successfully',
+            ]);
+        }
+    }
+
+
+    public function edit($id)
+    {
+        $category = Category::find($id);
+        if($category)
+        {
+            return response()->json([
+                'status' =>200,
+                'category' => $category,
+            ]);
+        }
+        else
+        {
+            return response()->json([
+                'status' =>200,
+                'message' => 'No category found',
+            ]);
+        }
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\CategoryController;
+
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 
 //isAPIAdmin
-
 Route::post('register',[AuthController::class, 'register']);
 Route::post('login',[AuthController::class,'login']);
 Route::middleware(['auth:sanctum','isAPIAdmin'])->group(function() {
@@ -15,15 +20,14 @@ Route::middleware(['auth:sanctum','isAPIAdmin'])->group(function() {
       return response()->json(['message' => 'You are in', 'status'=> 200],200);
    });
 
-   //Route::post('logout',[AuthController::class,'logout']);
+   Route::get('view-category',[CategoryController::class,'index']);
+   Route::post('store-category',[CategoryController::class,'store']);
+   Route::get('edit-category/{id}',[CategoryController::class,'edit']);
 });
-//Route::post('logout',[AuthController::class,'logout']);
+
 Route::middleware(['auth:sanctum'])->group(function() {
 
    Route::post('logout',[AuthController::class,'logout']);
 
 });
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
